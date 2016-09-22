1c714188cde447dfb958eb17893625c73a25fdcc
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-22 12:43:23 +0200

    Display content on home page

--- src/imports/startup/client/routes.jsx
+++ src/imports/startup/client/routes.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { mount } from 'react-mounter';
 
+import { MainLayout } from '../../ui/layouts/MainLayout.jsx';
+import HomeContainer from '../../ui/pages/home/HomeContainer.jsx';
+
 FlowRouter.route('/', {
   name: 'home',
   action() {
--- src/imports/ui/layouts/MainLayout.jsx
+++ src/imports/ui/layouts/MainLayout.jsx
@@ -1,9 +1,10 @@
 import React, {PropTypes} from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
+// import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 
+import HomeContainer from '../pages/home/HomeContainer.jsx';
 
-export const  = (props) => {
+export const MainLayout = (props) => {
   return (<div>MainLayout</div>);
 }
 
