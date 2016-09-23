8729eba5805d74970bb36249f138df0a80980946
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-23 08:14:35 +0200

    Publish and subscribe to user first and last name

--- src/imports/ui/pages/profile/ProfileContainer.jsx
+++ src/imports/ui/pages/profile/ProfileContainer.jsx
@@ -3,8 +3,9 @@ import { Meteor } from 'meteor/meteor';
 
 import Profile from './Profile.jsx';
 
-export default createContainer(() => (
-  {
+export default createContainer(() => {
+  Meteor.subscribe('Meteor.users.name');
+  return {
     currentUser: Meteor.user(),
-  }
-), Profile);
+  };
+}, Profile);
