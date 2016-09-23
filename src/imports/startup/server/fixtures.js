11cfd2f16e5f39b0e15d53a5c906787675906e7a
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-23 09:20:08 +0200

    Posts collection and fixtures

--- src/imports/startup/server/fixtures.js
+++ src/imports/startup/server/fixtures.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { faker } from 'meteor/practicalmeteor:faker';
 import { Accounts } from 'meteor/accounts-base';
 
+import { Posts } from '../../api/posts/posts.js';
+
 // only for development
 Accounts.onCreateUser((options, user) => {
   const newUser = user;
@@ -17,6 +19,34 @@ Meteor.startup(() => {
         username: `user${i}`,
         email: `user${i}@example.com`,
         password: 'postme',
+      });
+    }
+  }
+  if (Posts.find().count() < 100) {
+    const users = Meteor.users.find();
+    // eslint-disable-next-line no-underscore-dangle
+    const userIds = users.map((user) => user._id);
+    const usernames = users.map((user) => user.username);
+
+    const makeComments = () => {
+      const comments = [];
+      for (let j = 0; j < faker.random.number(5); j++) {
+        comments.push({
+          user: faker.random.arrayElement(usernames),
+          createdAt: new Date(),
+          title: faker.lorem.sentence(),
+        });
+      }
+      return comments;
+    };
+
+    for (let i = 1; i <= 100; i++) {
+      Posts.insert({
+        userId: faker.random.arrayElement(userIds),
+        title: faker.lorem.sentence(),
+        text: faker.lorem.paragraph(),
+        votes: faker.random.number(100),
+        comments: makeComments(),
       });
     }
   }
--- src/server/main.js
+++ src/server/main.js
@@ -1,2 +1,3 @@
 import '../imports/startup/server/index.js';
 import '../imports/api/users/server/publications.js';
+import '../imports/api/posts/posts.js';
