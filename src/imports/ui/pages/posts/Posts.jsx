8af895e49be9bcee3ae1c270faa133c7b6db2682
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-24 21:52:25 +0200

    Remove posts container

--- src/imports/ui/pages/posts/Posts.jsx
+++ src/imports/ui/pages/posts/Posts.jsx
@@ -1,16 +1,13 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 const Posts = () => (
   <div>
+    {}
     <button>Add new post</button>
     <button onClick={() => FlowRouter.go('posts.public')}>Public posts</button>
     <button onClick={() => FlowRouter.go('posts.private')}>Private posts</button>
   </div>
 );
-
-Posts.propTypes = {
-  currentUser: PropTypes.object,
-};
 
 export default Posts;
