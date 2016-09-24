a2b2b26c60abec59f7774a4d9691a4a04b312d4b
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-24 22:17:30 +0200

    Protect user private posts route

--- src/imports/ui/pages/posts/private/UserPrivatePosts.jsx
+++ src/imports/ui/pages/posts/private/UserPrivatePosts.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import PostListWrapper from '../../../components/posts/PostListWrapper.jsx';
 
-const UserPrivatePosts = () => (
-  <div>
-    <PostListWrapper
-      perPage={5}
-      sub={'Posts.user.private'}
-    />
-  </div>
-);
-export default UserPrivatePosts;
+class UserPrivatePosts extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.currentUser) FlowRouter.go('home');
+  }
+  render() {
+    return (
+      <div>
+        <PostListWrapper
+          perPage={5}
+          sub={'Posts.user.public'}
+        />
+      </div>
+    );
+  }
+}
+
+export default createContainer(() => (
+  {
+    currentUser: Meteor.user(),
+  }
+), UserPrivatePosts);
