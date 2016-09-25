9f7d3f9bd49ee888c302fa3493b9cf96edcfcece
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-25 23:55:27 +0200

    Users can post comments

--- src/imports/api/posts/methods.js
+++ src/imports/api/posts/methods.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
@@ -18,6 +19,29 @@ export const vote = new ValidatedMethod({
     Posts.update(postId, {
       $inc: { votes: 1 },
       $addToSet: { voters: this.userId },
+    });
+  },
+});
+
+export const newComment = new ValidatedMethod({
+  name: 'posts.newComment',
+  validate: new SimpleSchema({
+    postId: { type: String },
+    text: { type: String },
+  }).validator(),
+  run({ postId, text }) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized',
+        'You must be loged in to preform this action');
+    }
+    const comment = {
+      _id: Random.id(),
+      user: Meteor.users.findOne(this.userId).username,
+      createdAt: new Date(),
+      text,
+    };
+    Posts.update(postId, {
+      $addToSet: { comments: comment },
     });
   },
 });
--- src/imports/ui/components/comments/NewCommentForm.jsx
+++ src/imports/ui/components/comments/NewCommentForm.jsx
@@ -1,11 +1,30 @@
 import React, { Component, PropTypes } from 'react';
+import { newComment } from '../../../api/posts/methods.js';
 
 export default class NewCommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.postNewComment = this.postNewComment.bind(this);
+  }
+  postNewComment(e) {
+    e.preventDefault();
+    const input = document.getElementById('newCommentText');
+    const text = input.value.trim();
+    newComment.call({
+      postId: this.props.postId,
+      text,
+    }, (err) => {
+      if (err) {
+        // handle the error here
+      }
+      input.value = '';
+    });
+  }
   render() {
     return (
-      <form>
-        <label>Comment:</label><input type="text" />
-        <button>Submit</button>
+      <form onSubmit={this.postNewComment}>
+        <label>Comment:</label><input id="newCommentText" type="text" required />
+        <button type="submit">Submit</button>
       </form>
     );
   }
