5028930f8654d37989b0d2d47cdd738054e8f1c8
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-25 15:49:18 +0200

    Remove users collection from posts container

--- src/imports/ui/components/posts/PostListContainer.jsx
+++ src/imports/ui/components/posts/PostListContainer.jsx
@@ -9,17 +9,14 @@ export default createContainer((params) => {
   const handle = Meteor.subscribe(sub, limit);
   const loading = !handle.ready();
   let posts = [];
-  let users = [];
   if (!loading) {
     posts = Posts.find().fetch();
-    users = Meteor.users.find().fetch();
     if (posts.length < limit) {
       params.hitLimitFunc();
     }
   }
   return {
     posts,
-    users,
     loading,
     handle,
     skip,
