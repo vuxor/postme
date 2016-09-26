a9ec8edd056726700ea976c8d5720b40d3323082
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-27 00:08:27 +0200

    Add moment.js and use it in comment component

--- src/imports/ui/components/comments/Comment.jsx
+++ src/imports/ui/components/comments/Comment.jsx
@@ -1,8 +1,13 @@
 import React, { PropTypes } from 'react';
+import { moment } from 'meteor/momentjs:moment';
 
 const Comment = (props) => (
   <div className="card-panel">
-    <div className="comment-header">{props.comment.user}, on</div>
+    <div
+      className="comment-header grey-text text-lighten-1"
+    >
+      {props.comment.user}, posted {moment(props.comment.createdAt).fromNow()}
+    </div>
     <div className="comment-body">{props.comment.text}</div>
   </div>
 );
