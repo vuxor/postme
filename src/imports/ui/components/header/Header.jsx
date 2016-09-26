bba76c0310734ccc1432b622931613b563ba821c
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-26 20:26:15 +0200

    Header mobile menu

--- src/imports/ui/components/header/Header.jsx
+++ src/imports/ui/components/header/Header.jsx
@@ -1,42 +1,68 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import AccountsUIWrapper from '../accounts/AccountsUIWrapper.jsx';
 
-const Header = (props) => {
-  let list;
-  if (props.currentUser) {
-    list = (
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a onClick={() => FlowRouter.go('posts.public.best')}>Hall of fame</a></li>
-        <li><a onClick={() => FlowRouter.go('profile')}>Profile</a></li>
-        <li><a onClick={() => FlowRouter.go('posts.public')}>Public posts</a></li>
-        <li><a onClick={() => FlowRouter.go('posts.private')}>Private posts</a></li>
-        <li><a><AccountsUIWrapper /></a></li>
-      </ul>
-    );
-  } else {
-    list = (
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a onClick={() => FlowRouter.go('posts.public.best')}>Hall of fame</a></li>
-        <li><a><AccountsUIWrapper /></a></li>
-      </ul>
+export default class Header extends Component {
+  componentDidMount() {
+    $('.button-collapse').sideNav();
+  }
+  render() {
+    let list;
+    let sideMenu;
+    if (this.props.currentUser) {
+      list = (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a onClick={() => FlowRouter.go('posts.public.best')}>Hall of fame</a></li>
+          <li><a onClick={() => FlowRouter.go('profile')}>Profile</a></li>
+          <li><a onClick={() => FlowRouter.go('posts.public')}>Public posts</a></li>
+          <li><a onClick={() => FlowRouter.go('posts.private')}>Private posts</a></li>
+          <li><a><AccountsUIWrapper /></a></li>
+        </ul>
+      );
+      sideMenu = (
+        <ul id="mobile-nav" className="side-nav">
+          <li><a onClick={() => FlowRouter.go('posts.public.best')}>Hall of fame</a></li>
+          <li><a onClick={() => FlowRouter.go('profile')}>Profile</a></li>
+          <li><a onClick={() => FlowRouter.go('posts.public')}>Public posts</a></li>
+          <li><a onClick={() => FlowRouter.go('posts.private')}>Private posts</a></li>
+          <li><a><AccountsUIWrapper /></a></li>
+        </ul>
+      );
+    } else {
+      list = (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a onClick={() => FlowRouter.go('posts.public.best')}>Hall of fame</a></li>
+          <li><a><AccountsUIWrapper /></a></li>
+        </ul>
+      );
+      sideMenu = (
+        <ul id="mobile-nav" className="side-nav">
+          <li><a onClick={() => FlowRouter.go('posts.public.best')}>Hall of fame</a></li>
+          <li><a><AccountsUIWrapper /></a></li>
+        </ul>
+      );
+    }
+    return (
+      <div className="navbar-fixed">
+        <nav className="main-nav">
+          <div className="nav-wrapper">
+            <a onClick={() => FlowRouter.go('home')} className="brand-logo">Postme</a>
+            <a
+              data-activates="mobile-nav"
+              className="button-collapse"
+            >
+              <i className="material-icons">menu</i>
+            </a>
+            {list}
+            {sideMenu}
+          </div>
+        </nav>
+      </div>
     );
   }
-  return (
-    <div className="navbar-fixed">
-      <nav className="main-nav">
-        <div className="nav-wrapper">
-          <a onClick={() => FlowRouter.go('home')} className="brand-logo">Postme</a>
-          {list}
-        </div>
-      </nav>
-    </div>
-  );
-};
+  }
 
 Header.propTypes = {
   currentUser: PropTypes.object,
 };
-
-export default Header;
