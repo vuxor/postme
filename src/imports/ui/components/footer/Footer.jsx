92741196b7738bd4dbb41fe0f15cc9a629352609
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-26 12:53:22 +0200

    Footer modal markup

--- src/imports/ui/components/footer/Footer.jsx
+++ src/imports/ui/components/footer/Footer.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
+import { Materialize } from 'meteor/materialize:materialize';
 
 import PostForm from '../posts/PostForm.jsx';
 
 export default class Footer extends Component {
+  componentDidMount() {
+    $('.tooltipped').tooltip({ delay: 50 });
+  }
   showModal() {
-    $('#postModal').openModal();
+    if (Meteor.userId()) {
+      $('#postModal').openModal();
+    } else {
+      Materialize.toast('Please login first', 4000);
+    }
   }
   render() {
     return (
       <div>
         <a
+          data-position="left"
+          data-delay="50"
+          data-tooltip="Add new post"
           onClick={this.showModal}
-          className="modal-trigger btn-floating btn-large waves-effect waves-light red"
+          className="tooltipped
+          add-new-post modal-trigger btn-floating btn-large waves-effect waves-light red"
         >
           <i className="material-icons">add</i>
         </a>
 
         <div id="postModal" className="modal">
+          <div className="row">
+            <div className="col s12">
+              <h3>Add new post</h3>
+            </div>
+          </div>
           <PostForm />
         </div>
       </div>
