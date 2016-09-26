8c566489f9192f385fe8fc60d005da39f9d1edaa
radeljic_ivan@hotmail.com <radeljicivan85@gmail.com>
2016-09-26 20:41:49 +0200

    Home page header

--- src/imports/ui/pages/home/Home.jsx
+++ src/imports/ui/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import PostListWrapper from '../../components/posts/PostListWrapper.jsx';
 
 const Home = () => (
   <div>
+    <h2 className="header">Latest posts</h2>
     <PostListWrapper
       perPage={10}
       sub={'Posts.public'}
